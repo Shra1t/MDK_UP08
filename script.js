02aaf35ec820152cfa47e57ff501ba6b864be428
Shra1t <165365544+Shra1t@users.noreply.github.com>
2024-11-30 13:26:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector(".menu");
+let menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", function(e){
+    e.preventDefault();
+    menu.classList.toggle("show-menu");
+})
+window.addEventListener('resize', function(){
+    if(this.window.innerWidth > 375 && menu.classList.contains('show-menu')){
+        menu.classList.remove('show-menu');
+    }
+})
